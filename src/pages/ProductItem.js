c56e9e0ee1041c01e30c5f48822668ded31420a2
NattyDCI <nbavutti@gmail.com>
2022-09-01 14:44:33 +0200

    some styling was done

---
+++ src/pages/ProductItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Product() {
+  return (
+    <div>
+        <h1>Single Product Page</h1>
+        <p>something</p>
+        <p>learning merge git</p>
+        
+        </div>
+  )
+}
