a155e699e7ac86aa4f8c11ae13f8488584320de9
nat-laz <lazarev.n@yahoo.com>
2022-09-05 12:03:09 +0200

    first step functionality

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from "../context/AppContext";
+
 
 export default function Cart() {
+  const {cart, setCart}  = useContext(AppContext)
   return (
     <div>
       <h2>update cart component</h2>
+      <div>
+        {cart.map(item=>(
+          <div>
+            {item.title}
+          </div>
+        ))}
+      </div>
       </div>
   )
 }
