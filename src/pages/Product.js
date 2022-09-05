fc8e8e5ca2ce4c7a81f59e60f10ad3113adfd7c7
nat-laz <lazarev.n@yahoo.com>
2022-09-05 12:24:24 +0200

    instead of useParams() -> useLocation()

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -3,33 +3,33 @@ import { useLocation, useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 
 export default function Product() {
-  // const location = useLocation();
+  const location = useLocation();
 
-  const { products, setCart, cart } = useContext(AppContext);
-  const { id } = useParams();
-  const singleProduct = products.find((item) => item.id === +id); //used "+" to convert in number
-  // // console.log(id);
-  // const [product, setProduct] = useState({});
+  const {  setCart, cart } = useContext(AppContext);
+  // const { id } = useParams();
+  // const singleProduct = products.find((item) => item.id === +id); //used "+" to convert in number
+  // // // console.log(id);
+  // // const [product, setProduct] = useState({});
 
-  // useEffect(() => {
-  //   const singleProduct = products.find((item) => item.id === +id); //used "+" to convert in number
-  //   setProduct(singleProduct);
-  // }, []);
-  // //  console.log(product)
+  // // useEffect(() => {
+  // //   const singleProduct = products.find((item) => item.id === +id); //used "+" to convert in number
+  // //   setProduct(singleProduct);
+  // // }, []);
+  // // //  console.log(product)
  const addToCart = () => {
 
-    setCart([...cart, singleProduct])
+    setCart([...cart, location.state])
  }
 
   return (
     <div>
       <h1>single product page</h1>
-      {singleProduct && (
+      {location.state && (
         <>
           {" "}
-          <img src={singleProduct.image} alt="" width={200} />
-          <h1>{singleProduct.title}</h1>
-          <h3>{singleProduct.description}</h3>
+          <img src={location.state.image} alt="" width={200} />
+          <h1>{location.state.title}</h1>
+          <h3>{location.state.description}</h3>
           <button onClick={addToCart}>add to cart</button>
         </>
       )}
