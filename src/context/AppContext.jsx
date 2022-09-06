0aaa4b0b2e54383b9f751515ecf226094f8a16d5
nat-laz <lazarev.n@yahoo.com>
2022-09-06 09:49:08 +0200

    Cart - Work in progress:
    - build cart content;
    - added Functionality: addProduct, deleteProduct, incrementQuantity/decrement;
    - disabled incrementButton;
    - added TotalAmout;

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -4,13 +4,43 @@ import productArray from "../data/productArray.json";
 const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
- 
- const [products, setProducts] = useState(productArray);
- const [cart, setCart] = useState([])
+  const [products, setProducts] = useState(productArray);
+  const [cart, setCart] = useState([]);
 
- const defaultContext = {
-  products, setProducts, cart, setCart
+  //Cart Functionality
+  const deleteItemCart = (id) => {
+    const filtredCart = cart.filter((product) => product.id !== id);
+    setCart(filtredCart);
+  };
+
+ const incrementQuantity = (item)=> {
+    setCart(cart.map(product => {
+      if(item.id === product.id){
+        product.quantity++
+        return product
+      } else{
+        return product
+      }
+    }))
  }
+const decrementQuantity = (item)=> {
+  setCart(cart.map(product => {
+    if(item.id === product.id){
+      product.quantity--
+      return product
+    } else{
+      return product
+    }
+  }))
+ }
+  const defaultContext = {
+    products,
+    setProducts,
+    cart,
+    setCart,
+    deleteItemCart,
+    incrementQuantity, decrementQuantity
+  };
 
   return (
     <AppContext.Provider value={defaultContext}>{children}</AppContext.Provider>
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,11 +1,11 @@
-import React, { useContext, useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import React, { useContext } from "react";
+import { useLocation } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 
 export default function Product() {
   const location = useLocation();
 
-  const {  setCart, cart } = useContext(AppContext);
+  const { setCart, cart } = useContext(AppContext);
   // const { id } = useParams();
   // const singleProduct = products.find((item) => item.id === +id); //used "+" to convert in number
   // // // console.log(id);
@@ -16,17 +16,23 @@ export default function Product() {
   // //   setProduct(singleProduct);
   // // }, []);
   // // //  console.log(product)
- const addToCart = () => {
-
-    setCart([...cart, location.state])
- }
+  const addToCart = () => {
+    const findedItem = cart.find(item=> item.id === location.state.id  )
+    if(findedItem ){
+      findedItem.quantity++
+      return setCart([...cart])
+    } else {
+      location.state.quantity = 1
+      return setCart([...cart, location.state]);
+    }
+    
+  };
 
   return (
     <div>
       <h1>single product page</h1>
       {location.state && (
         <>
-          {" "}
           <img src={location.state.image} alt="" width={200} />
           <h1>{location.state.title}</h1>
           <h3>{location.state.description}</h3>
