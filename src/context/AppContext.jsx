5aa276dc96fdddc715199f78f9b6d58cda3831e3
Nathalia Padron <nbavutti@gmail.com>
2022-09-06 09:50:54 +0200

    -created the register Form -created a useState for user in the contextAPI

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -4,12 +4,12 @@ import productArray from "../data/productArray.json";
 const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
- 
+ const [user, setUser] = useState(null);
  const [products, setProducts] = useState(productArray);
  const [cart, setCart] = useState([])
 
  const defaultContext = {
-  products, setProducts, cart, setCart
+  products, setProducts, cart, setCart,user, setUser
  }
 
   return (
