ec4bf3dc201f579f2c0d9e7286b615b35a1b4905
Nathalia Padron <nbavutti@gmail.com>
2022-09-06 10:33:26 +0200

    -accepted changes from Natalie

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -4,48 +4,13 @@ import productArray from "../data/productArray.json";
 const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
-
- const [user, setUser] = useState(null);
+ 
  const [products, setProducts] = useState(productArray);
  const [cart, setCart] = useState([])
 
-  //Cart Functionality
-  const deleteItemCart = (id) => {
-    const filtredCart = cart.filter((product) => product.id !== id);
-    setCart(filtredCart);
-  };
-
- const incrementQuantity = (item)=> {
-    setCart(cart.map(product => {
-      if(item.id === product.id){
-        product.quantity++
-        return product
-      } else{
-        return product
-      }
-    }))
+ const defaultContext = {
+  products, setProducts, cart, setCart
  }
-const decrementQuantity = (item)=> {
-  setCart(cart.map(product => {
-    if(item.id === product.id){
-      product.quantity--
-      return product
-    } else{
-      return product
-    }
-  }))
-
-  const defaultContext = {
-    products,
-    setProducts,
-    cart,
-    setCart,
-    deleteItemCart,
-    incrementQuantity,
-    decrementQuantity,
-    user, 
-    setUser
-  };
 
   return (
     <AppContext.Provider value={defaultContext}>{children}</AppContext.Provider>
