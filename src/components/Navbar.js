2e9f165c03115c841596aa572d31f9411b165190
nat-laz <lazarev.n@yahoo.com>
2022-09-07 10:13:35 +0200

    Update:
    *LoginPage
    
    - Login logic : Check email and password matching with registerUser;
    - npm install react-hot-toast
    - added Alert messages
    
    *RegisterPage
    
    -changed path to HomePage;
    
    -a message on successful registration has been added
    
    *NavbarComponent
    
    - Link Profile: value: user.firstName;
    - added link: about, cataloge;
    - changed CartIcon;
    - started with styling
    
    *ProductPage
    
    -added functionality to arrows icons;
    
    -added Rate from antdesign+ count;
    
    -styling - done;
    
    *CartPage
    
    -small styling changes;

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,54 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
+import { RiShoppingCartLine } from "react-icons/ri";
+import "./Navbar.css";
 
 export default function Navbar() {
-  const {cart, user} = useContext(AppContext)
+  const { cart, user } = useContext(AppContext);
   return (
-    <ul
-      style={{
-        display: "flex",
-        justifyContent: "space-around",
-        listStyleType: "none",
-        color: "black",
-        textDecoration: "none",
-        alignItems: "center",
-      }}
-    >
-      <div>
-       <Link to="/"><img src={require("../images/furniture.png")} alt="" width="150" /></Link>
+    <ul className="Navbar">
+      <div className="nav-logo">
+        <Link to="/">
+          <img src={require("../images/furniture.png")} alt="" width="150" />
+        </Link>
       </div>
-      {user ?  <li>
-        <Link to="/profile">Profile</Link>
-      </li> : <>
-      {/* <li>
-        <Link to="/register">Register</Link>
-      </li> */}
-      <li>
-        <Link to="/login">Login</Link>
-      </li> </>}
-      
-      
-      <li>
-        <Link to="/products">Products</Link>
-      </li>
-      <li>
-        <Link to="/cart">Cart <sup>{cart.length}</sup></Link>
-      </li>
-      <li>
-        <Link to="/contactus">Contact Us</Link>
-      </li>
-     
+
+      <div className="nav-links">
+        <li>
+          <Link to="/about">about</Link>
+        </li>
+
+        <li>
+          <Link to="/cataloge">cataloge</Link>
+        </li>
+        <li>
+          <Link to="/products">products</Link>
+        </li>
+
+        <li>
+          <Link to="/contactus">contact</Link>
+        </li>
+      </div>
+
+      <div className="nav-log-cart">
+        {user ? (
+          <li>
+            <Link to="/profile">{user.firstName.toUpperCase()}</Link>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">LOG IN</Link>
+            </li>
+          </>
+        )}
+        <li>
+          <Link to="/cart">
+            <RiShoppingCartLine /> <sup>{cart.length}</sup>
+          </Link>
+        </li>
+      </div>
     </ul>
   );
 }
