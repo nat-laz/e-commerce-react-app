68a8ad4925c3c6eab8d7e3ffc6f8eb0747a8b4eb
nat-laz <lazarev.n@yahoo.com>
2022-09-09 17:51:51 +0200

    - adding Wishlist Icon; - toggle liked/unliked product - still in process; - add/delete in WishList functionality

--- src/pages/ContactUs.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ContactUs() {
-  return (
-    <div><h1>ContactUs</h1></div>
-  )
-}
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,20 +1,33 @@
-import React,{ useContext } from "react";
+import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import { Navigate } from "react-router-dom";
-
+import "../style/Profile.css";
+import Wishlist from "../component/Wishlist";
 
 export default function Profile() {
   const { user, setUser } = useContext(AppContext);
   if (user) {
     return (
-      <div>
-        <h1>
-          {user.firstName}, {user.lastName}
-        </h1>
-        <p>{user.email}</p>
-        <p>{user.city}</p>
-        <p>{user.phone}</p>
-        <button onClick={() => setUser(null)}>SIGN OUT</button>
+      <div className="Profile">
+        <div className="profile-info">
+          <p>PROFILE</p>
+          <h4>
+            {user.lastName.toUpperCase()} {user.firstName.toUpperCase()}
+          </h4>
+          <p>{user.email}</p>
+          <h4>ACCOUNT</h4>
+          <p>EMAIL, PASSWORD</p>
+          <h4>ADDRESSES</h4>
+          <p>SHIPPING AND BILLING ADDRESSES</p>
+          <h4>WALLET</h4>
+          <p>PAYMENT METHODS</p>
+          <button onClick={() => setUser(null)}>SIGN OUT</button>
+        </div>
+
+        <div className="wishlist-wrapper">
+          <p>YOUR WISHLIST</p>
+          <Wishlist/>
+        </div>
       </div>
     );
   } else {
